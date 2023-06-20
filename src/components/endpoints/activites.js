c422914226e19467d1d869f3a26f52da27230607
JorgeR-Hi <jrjorgemessi@gmail.com>
2023-06-20 14:57:36 -0500

    Routines tab sort of works

---
+++ src/components/endpoints/activites.js
@@ -0,0 +1,18 @@
+const BASE_URL = `https://fitnesstrac-kr.herokuapp.com/api`
+
+const fetchAllActivities = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/activities`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      const result = await response.json();
+  
+      console.log(result);
+      return result
+    } catch (err) {
+      console.error(err);
+    }
+  }
